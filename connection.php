98b025c972a19fe8202d03b9c4ac4eb02087b622
Pai do Max <ricardoaugusto.cordeiro@gmail.com>
2017-12-01 01:08:35 -0200

    Alterando os links de cadastro e login da página index. Adicionado o arquivo de conexão ao banco de dados

---
+++ connection.php
@@ -0,0 +1,9 @@
+<?php
+
+$databaseHost = 'localhost';
+$databaseName = 'ihc';
+$databaseUsername = 'root';
+$databasePassword = '';
+
+$mysqli = mysqli_connect($databaseHost, $databaseUsername, $databasePassword, $databaseName);
+?>
--- index.php
+++ index.php
@@ -1,37 +1,64 @@
 <!DOCTYPE html>
-<html>
-    <head>
-        <meta charset="utf-8">
-        <title>Repositorio de TI em Libras</title>
-        <link href="css/bootstrap.min.css" rel="stylesheet">
-    </head>
-    <body>
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-        <script src="js/bootstrap.min.js"></script>
+<html lang="pt-br">
+  <head>
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>LibrasTec</title>
 
-        <div class="container-fluid">
-                <div class="row">
-                    <div class="col-md-4">
-                        <p>Lorem ipsum dolor sit amet</p>
-                    </div>
-                    <div class="col-md-3">
-                        <a href="#">Login</a>
-                        <a href="#">Cadastrar</a>
-                    </div>
-                </div>
-                <div class="text-center"
-                      <p>TESTE</p>
-                </di>
-                <div class="center-block">
-                  <div class="center-block">
-                    <form class="" action="index.html"class="barra_pesquisa" method="post">
-                      <input type="search" name="pesquisa" value="">
-                      <input type="submit" name="enviar"  value="Pesquisar">
-                    </form>
+    <!-- Bootstrap -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
+    <!-- Fim - Bootstrap -->
+
+    <!-- jQuery -->
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+    <!-- Fim - jQuery -->
+
+  </head>
+  <body>
+    <nav class="navbar" id="home">
+      <div class="container-fluid">
+         <ul class="nav navbar-nav navbar-right">
+         <li class="gly-login"><a data-toggle="modal" data-target="#login"><span class="glyphicon glyphicon-user"></span> Login</a></li>
+         <li class="gly-login"><a data-toggle="modal" data-target="#login"><span class="fa fa-sign-in"></span> Cadastre-se</a></li>
+       </ul>
+      </div>
+    </nav>
+    <!-- Modal -->
+      <div class="modal fade" id="login" role="dialog">
+        <div class="modal-dialog">
+          <!-- Modal conteúdo-->
+          <div class="modal-content">
+            <div class="modal-header">
+              <button type="button" class="close" data-dismiss="modal">&times;</button>
+              <h4 class="modal-title text-center">Login</h4>
+            </div>
+            <div class="modal-body">
+              <form action="login.php" method="post" name="form2">
+                  <div class="form-group">
+                    <input type="email" class="form-control inscricao-input" id="email" name="email" placeholder="E-mail" required>
                   </div>
-                </div>
+                  <div class="form-group">
+                    <input type="password" class="form-control inscricao-input" id="senha" name="senha" placeholder="Senha" required>
+                  </div>
+                  <button type="submit" name="submit" class="btn btn-me-inscrever center-block">Entrar</button>
+              </form>
+            </div>
+          </div>
         </div>
+      </div>
+      <!-- Fim Modal -->
 
-
-    </body>
+      <div class="container">
+        <form action="pesquisa.php" method="post">
+            <label class="col-sm-2 col-form-label">Busque uma Palavra:</label>
+              <input type="text" id="procurar" name="procurar" style="width:108%;" class="form-control" placeholder="Procurar"/>
+              <input type="submit" class="btn btn-primary" value="Procurar"/>
+          </form>
+        </div>
+  </body>
 </html>
