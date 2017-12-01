6865c92acf65f6b0e0383d2f5d42c5bf3924eba2
Pai do Max <ricardoaugusto.cordeiro@gmail.com>
2017-12-01 16:04:23 -0200

    alteração da index

--- index.php
+++ index.php
@@ -3,9 +3,8 @@ session_start();
 ?>
 <html lang="pt-br">
   <head>
-  <meta charset="utf-8">
+    <meta charset="utf-8" name="viewport" content="width=device-width, initial-scale=1">
     <title>LibrasTec</title>
-
     <link href="css/bootstrap.min.css" rel="stylesheet">
     <link href="css/font-awesome.min.css" rel="stylesheet">
     <link href="css/font-awesome.css" rel="stylesheet">
@@ -13,15 +12,12 @@ session_start();
 
   </head>
   <body>
-
-    <nav class="navbar navbar-right" id="home">
-         <a href="#login" class="btn btn-primary"><span class="glyphicon glyphicon-user"></span> Login</a>
-         <a class="btn btn-primary" href="cadastro.php"><span class="fa fa-sign-in"></span> Cadastre-se</a>
-      </div>
+    <div class="float-right">
+        <button class="btn btn-primary btn-lg" id="myBtn"><span class="fa fa-user-o" aria-hidden="true"></span> Login</button>
+        <a class="btn btn-primary btn-lg" href="cadastro.php"><span class="fa fa-sign-in" aria-hidden="true"></span> Cadastre-se</a>
     </div>
-    </nav>
-    <!-- Modal -->
-      <div class="modal fade" id="login" role="dialog">
+      <!-- Modal -->
+      <div class="modal fade" id="#myModal" role="dialog">
         <div class="modal-dialog">
           <!-- Modal conteúdo-->
           <div class="modal-content">
@@ -30,7 +26,7 @@ session_start();
               <h4 class="modal-title text-center">Login</h4>
             </div>
             <div class="modal-body">
-              <form action="login.php" method="post" name="form2">
+              <form role="form" action="" method="post" name="form2">
                   <div class="form-group">
                     <input type="email" class="form-control inscricao-input" id="email" name="email" placeholder="E-mail" required>
                   </div>
@@ -44,7 +40,7 @@ session_start();
         </div>
       </div>
       <!-- Fim Modal -->
-
+      <div class="central" style="position: absolute; height: 50%; top: 38%; width: 100%;">
       <div class="container">
         <div class="row">
           <div class="col-md-2"></div>
@@ -61,26 +57,34 @@ session_start();
         <div class="row">
           <div class="col-md-4"></div>
           <div class="col-md-4">
-          <p style="font-size:40px;">Faça uma busca:</p>
+          <p style="text-align:center; font-size:30px;"><b>Faça uma busca</b></p>
         </div>
       <div class="col-md-4"></div>
         </div>
         <div class="row">
           <div class="col-md-2"></div>
           <div class="col-md-8">
-            <div id="custom-search-input">
+
                 <div class="input-group col-md-12">
                     <input type="text" class="form-control input-lg" placeholder="Buscar" />
                     <span class="input-group-btn">
                         <button class="btn btn-primary btn-lg" type="button">
-                            <i class="glyphicon glyphicon-search"></i>
+                            <span class="fa fa-search" aria-hidden="true"></span>
                         </button>
                     </span>
                 </div>
-            </div>
+              </div>
           <div class="col-md-2"></div>
       </div>
     </div>
+  </div>
 		<script src="js/bootstrap.min.js"></script>
+    <script>
+      $(document).ready(function(){
+          $("#myBtn").click(function(){
+              $("#myModal").modal();
+          });
+      });
+    </script>
   </body>
 </html>
