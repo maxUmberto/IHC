8d2108ff188128bade7dea9c9a880995ecfb80b2
Faustop <guiguk9@gmail.com>
2017-11-30 23:39:21 -0200

    index

--- index.php
+++ index.php
@@ -8,24 +8,27 @@
     <body>
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
         <script src="js/bootstrap.min.js"></script>
-        <div class="container">
-                <div class="header">
-                    <div class="titulo_header">
+
+        <div class="container-fluid">
+                <div class="row">
+                    <div class="col-md-4">
                         <p>Lorem ipsum dolor sit amet</p>
                     </div>
-                    <div class="login_header">
+                    <div class="col-md-3">
                         <a href="#">Login</a>
                         <a href="#">Cadastrar</a>
                     </div>
                 </div>
-                <div class="titulo">
-                    <p>TESTE</p>
-                </div>
-                <div class="pesquisa">
-                            <form class="" action="index.html"class="barra_pesquisa" method="post">
-                                    <input type="search" name="pesquisa" value="">
-                                    <input type="submit" name="enviar"  value="Pesquisar">
-                            </form>
+                <div class="text-center"
+                      <p>TESTE</p>
+                </di>
+                <div class="center-block">
+                  <div class="center-block">
+                    <form class="" action="index.html"class="barra_pesquisa" method="post">
+                      <input type="search" name="pesquisa" value="">
+                      <input type="submit" name="enviar"  value="Pesquisar">
+                    </form>
+                  </div>
                 </div>
         </div>
 
