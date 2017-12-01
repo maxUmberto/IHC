9e8ccb0e1a866307e2e91a17f692dab916e2cc46
Max Santos <max.umberto97@gmail.com>
2017-11-30 23:21:43 -0200

    Páginda de cadastro

---
+++ cadastro.php
@@ -0,0 +1,44 @@
+<?php
+
+?>
+
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>Repositorio de TI em Libras - Cadastro</title>
+  <link href="css/bootstrap.min.css" rel="stylesheet">
+  <link href="css/font-awesome.min.css" rel="stylesheet">
+  <link href="css/font-awesome.css" rel="stylesheet">
+  <link href="css/estilo.css" rel="stylesheet">
+</head>
+
+<body>
+  <header class="ccheader">
+    <h1>Formulário de cadastro</h1>
+  </header>
+  <div class="wrapper">
+      <form method="post" action="" class="ccform">
+      <div class="ccfield-prepend">
+          <span class="ccform-addon"><i class="fa fa-user fa-2x"></i></span>
+          <input class="ccformfield" type="text" placeholder="Nome completo" required>
+      </div>
+      <div class="ccfield-prepend">
+          <span class="ccform-addon"><i class="fa fa-envelope fa-2x"></i></span>
+          <input class="ccformfield" type="email" placeholder="Email" required>
+      </div>
+      <div class="ccfield-prepend">
+          <span class="ccform-addon"><i class="fa fa-key fa-2x"></i></span>
+          <input class="ccformfield" type="password" placeholder="Senha" required>
+      </div>
+      <div class="ccfield-prepend">
+          <span class="ccform-addon"><i class="fa fa-key fa-2x"></i></span>
+          <input class="ccformfield" type="password" placeholder="Confirme sua senha" required>
+      </div>
+      <div class="ccfield-prepend">
+          <input class="ccbtn" type="Cadastrar" value="Submit">
+      </div>
+      </form>
+  </div>
+  <script src="js/bootstrap.min.cs"></script>
+</body>
+</html>
