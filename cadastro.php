9712d164ff9791893d37ebc5071c768265515dae
Faustop <guiguk9@gmail.com>
2017-12-01 05:58:37 -0200

    linkando o botão voltar da pagina cadastro para a pagina inicial

--- cadastro.php
+++ cadastro.php
@@ -1,4 +1,5 @@
 <?php
+// inicio da sessão
   session_start();
 ?>
 
@@ -6,6 +7,7 @@
 <head>
   <meta charset="utf-8">
   <title>LibrasTEC - Cadastro</title>
+  <!-- chamando o css do bootsrap -->
   <link href="css/bootstrap.min.css" rel="stylesheet">
   <link href="css/font-awesome.min.css" rel="stylesheet">
   <link href="css/font-awesome.css" rel="stylesheet">
@@ -13,46 +15,49 @@
 </head>
 
 <body>
+  <!--Div do titulo da pagina-->
   <header class="ccheader">
     <h1>Formulário de cadastro</h1>
   </header>
-  <div class="wrapper">
+  <div class="wrapper"><!-- Div container -->
 
       <form method="post" action="valida.php" class="ccform">
         <?php
-          if(isset($_SESSION['msg'])){
+              //Mensagens de erro de cadastro
+              if(isset($_SESSION['msg'])){
               echo $_SESSION['msg'];
               unset($_SESSION['msg']);
           }
         ?>
-      <div class="ccfield-prepend">
+      <div class="ccfield-prepend"><!--Campo do NOME -->
           <span class="ccform-addon" aria-hidden="true"><i class="fa fa-user fa-2x"></i></span>
           <input class="ccformfield" type="text" placeholder="Nome completo" name="nome">
       </div>
-      <div class="ccfield-prepend">
+      <div class="ccfield-prepend"><!-- Campo do E-MAIL -->
           <span class="ccform-addon" aria-hidden="true"><i class="fa fa-envelope fa-2x"></i></span>
           <input class="ccformfield" type="email" placeholder="Email" name="email">
       </div>
-      <div class="ccfield-prepend">
+      <div class="ccfield-prepend"><!-- Campo da senha -->
           <span class="ccform-addon" aria-hidden="true"><i class="fa fa-key fa-2x"></i></span>
           <input class="ccformfield" type="password" placeholder="Senha" name="senha">
       </div>
-      <div class="ccfield-prepend">
+      <div class="ccfield-prepend"><!--Campo da confirmação de senha -->
           <span class="ccform-addon"><i class="fa fa-key fa-2x"></i></span>
           <input class="ccformfield" type="password" placeholder="Confirme sua senha" name="senha2">
       </div>
+      <!-- Div dos botões-->
       <div class="ccfield-prepend row" style="margin-left:0px;">
         <div class="col-md-ofset-0"></div>
           <button type="submit" class="ccbtn">
             Cadastrar <i class="fa fa-sign-in" aria-hidden="true"></i>
           </button>
           <div class="col-md-0" style="margin-left:10px"></div>
-      <button type="button" class="ccbtn">
-        Voltar <i class="fa fa-undo" aria-hidden="true"></i>
+      <!--<button type="button" class="ccbtn">-->
+        <a href="index.php" class="ccbtn" style="text-decoration:none">Voltar <i class="fa fa-undo" aria-hidden="true"></i></a>
       </button>
-    </div>
+    </div><!-- fim da div dos botões-->
       </form>
-  </div>
+  </div><!-- fim da div container-->
   <script src="js/bootstrap.min.cs"></script>
 </body>
 </html>
