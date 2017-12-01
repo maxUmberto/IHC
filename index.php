1e683870bcde73333670470629e075bf16b94e8b
Faustop <guiguk9@gmail.com>
2017-11-30 22:28:22 -0200

    INDEX E O BOOTSTRAP

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Repositorio de TI em Libras</title>
+        <link rel="stylesheet" href="master.css">
+    </head>
+    <body>
+        <div class="container">
+                <div class="header">
+                    <div class="titulo_header">
+                        <p>Lorem ipsum dolor sit amet</p>
+                    </div>
+                    <div class="login_header">
+                        <a href="#">Login</a>
+                        <a href="#">Cadastrar</a>
+                    </div>
+                </div>
+                <div class="titulo">
+                    <p>TESTE</p>
+                </div>
+                <div class="pesquisa">
+                            <form class="" action="index.html"class="barra_pesquisa" method="post">
+                                    <input type="search" name="pesquisa" value="">
+                                    <input type="submit" name="enviar"  value="Pesquisar">
+                            </form>
+                </div>
+        </div>
+
+
+    </body>
+</html>
