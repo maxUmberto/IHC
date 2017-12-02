c2d545b523c727caf3eb7521aebfd83810a92cd7
Max Santos <max.umberto97@gmail.com>
2017-12-02 00:27:22 -0200

    Alterações no código de valida para que tanto o cadastro e login sejam validados na mesma página

--- valida.php
+++ valida.php
@@ -2,9 +2,11 @@
   session_start();
 
   $email = htmlspecialchars(trim($_POST['email']));
-  $nome = htmlspecialchars(trim($_POST['nome']));
   $senha = sha1(htmlspecialchars(trim($_POST['senha'])));
-  $senha2 = sha1(htmlspecialchars(trim($_POST['senha2'])));
+  if(isset($_POST['nome'])){
+    $nome = htmlspecialchars(trim($_POST['nome']));
+    $senha2 = sha1(htmlspecialchars(trim($_POST['senha2'])));
+  }
 
   $_SESSION['msg'] = '';
 
@@ -20,13 +22,19 @@ function validaEmail($email) {
     return false;
 }
 
-function validaNome($nome){
-  $nome2 = "/(?=^.{2,60}$)^[A-ZÀÁÂĖÈÉÊÌÍÒÓÔÕÙÚÛÇ][a-zàáâãèéêìíóôõùúç]+(?:[ ](?:das?|dos?|de|e|[A-Z][a-z]+))*$/";
+if(isset($nome)){
+  function validaNome($nome){
+    $nome2 = "/(?=^.{2,60}$)^[A-ZÀÁÂĖÈÉÊÌÍÒÓÔÕÙÚÛÇ][a-zàáâãèéêìíóôõùúç]+(?:[ ](?:das?|dos?|de|e|[A-Z][a-z]+))*$/";
 
-  if (preg_match($nome2, $nome))
+    if (preg_match($nome2, $nome))
     return true;
-  else
+    else
     return false;
+  }
+
+  if(!validaNome($nome)){
+    $_SESSION['msg'] .= "Nome inválido<br>";
+  }
 }
 
 function validaSenha($senha,$senha2){
@@ -36,19 +44,25 @@ function validaSenha($senha,$senha2){
   }else if(strlen($senha)<=5){
     return false;
     //echo 'senha curta';
-  }else if($senha != $senha2 ){
+  }
+  return true;
+}
+
+function comparaSenha($senha, $senha2){
+  if($senha != $senha2 ){
     return false;
     //echo 'senhas diferentes';
   }
   return true;
 }
 
-if(!validaSenha($senha,$senha2)){
+if(!validaSenha($senha){
   $_SESSION['msg'] .= "Senha inválida<br>";
-}
-
-if(!validaNome($nome)){
-  $_SESSION['msg'] .= "Nome inválido<br>";
+  if(isset($senha2)){
+    if(!comparaSenha($senha,$senha2)){
+      $_SESSION['msg'] .= "Senha inválida<br>";
+    }
+  }
 }
 
 if (!validaEmail($email)){
@@ -57,23 +71,30 @@ if (!validaEmail($email)){
 
 if(empty($_SESSION['msg'] == '')){
   $_SESSION['msg'] = '<div class="alert alert-danger" role="alert">'.$_SESSION['msg'].'</div>';
-  header('Location: cadastro.php');
-}else{
-  require_once('connection.php');
-
-  $sql = "SELECT * FROM usuarios WHERE email = '$email'";
-  $resultado = mysqli_query($mysqli,$sql);
-  if(mysqli_num_rows($resultado) == 0){
-    $sql = "INSERT INTO usuarios VALUES ('$email','$nome', '$senha')";
-
-    $query = $mysqli->query($sql);
-
-    $_SESSION['msg'] = '<div class="alert alert-success text-center" role="alert">Usuário cadastrado com sucesso</div>';
-    header('Location: index.php');
+  if(isset($nome)){
+    header('Location: cadastro.php');
   }
   else{
-    $_SESSION['msg'] = '<div class="alert alert-danger" role="alert"> Email já está sendo usado</div>';
-    header('Location: cadastro.php');
+    header('Location: index.php');
+  }
+}else{
+  if(isset($nome)){
+    require_once('connection.php');
+
+    $sql = "SELECT * FROM usuarios WHERE email = '$email'";
+    $resultado = mysqli_query($mysqli,$sql);
+    if(mysqli_num_rows($resultado) == 0){
+      $sql = "INSERT INTO usuarios VALUES ('$email','$nome', '$senha')";
+
+      $query = $mysqli->query($sql);
+
+      $_SESSION['msg'] = '<div class="alert alert-success text-center" role="alert">Usuário cadastrado com sucesso</div>';
+      header('Location: index.php');
+    }
+    else{
+      $_SESSION['msg'] = '<div class="alert alert-danger" role="alert"> Email já está sendo usado</div>';
+      header('Location: cadastro.php');
+    }
   }
 }
 ?>
